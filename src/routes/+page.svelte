dce2074066b94837d30ae1ed5e6c8b4da1983f6d
Mr-W-1 <99057212+Mr-W-1@users.noreply.github.com>
2023-03-21 21:35:28 +1100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -64,7 +64,7 @@
 
 <div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
 	<div>
-		<h0 class="text-2xl font-bold w-full text-center">Mr Wadsworth's New and Improved Python Bot 2.0</h0>
+		<h0 class="text-2xl font-bold w-full text-center">Mr Wadsworth's Bot 2.0</h0>
 		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
 	</div>
 	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
